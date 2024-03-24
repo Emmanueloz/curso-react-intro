ced3a6ebb42eac55ba06cb510903e26e6f83f6ce
EmmanuelOZ <davidozuna84@hotmail.com>
2024-03-24 17:25:55 -0600

    mensaje de todos los TODOs completados

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,7 +1,15 @@
 import "./TodoCounter.css";
+import PropTypes from "prop-types";
+
+TodoCounter.propTypes = {
+  total: PropTypes.number.isRequired,
+  completed: PropTypes.number.isRequired,
+};
 
 function TodoCounter({ total, completed }) {
-  return (
+  return total === completed ? (
+    <h1 className="TodoCounter">Has completado todos las tareas</h1>
+  ) : (
     <h1 className="TodoCounter">
       Has completado <span>{completed}</span> de <span>{total}</span> TODOS
     </h1>
